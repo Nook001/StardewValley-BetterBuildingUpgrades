c9893c70a084eca96e44fb3c426b97e45e593130
nook <1934285489lyz@gmail.com>
2025-10-08 14:40:36 +0800

    new feature

--- Utils/ContentManager/TextureManager.cs
+++ Utils/ContentManager/TextureManager.cs
@@ -10,10 +10,13 @@ public static partial class ContentManager
     {
         { "Buildings/Big Silo", "assets/Big_Silo.png" },
         { "Buildings/Deluxe Silo", "assets/Deluxe_Silo.png" },
+        { "Buildings/Grinding Silo", "assets/Grinding_Silo.png" },
         { "Buildings/Big Greenhouse", "assets/Big_Greenhouse.png" },
         { "Buildings/Deluxe Greenhouse", "assets/Deluxe_Greenhouse.png" },
         { "Buildings/Big Well", "assets/Big_Well.png" },
         { "Buildings/Big Stable", "assets/Big_Stable.png" },
+
+        { "Objects/FineHay", "assets/Fine_Hay.png" },
     };
 
     // Load the custom textures for the buildings
--- Utils/Data/ObjectData.cs
+++ Utils/Data/ObjectData.cs
@@ -15,11 +15,11 @@ public static partial class DataManager
         Description = ModEntry.Translation.Get("fine-hay.description"),
         Type = "Basic",
         Category = -81,          // category_greens Forage
-        Price = 10,               
+        Price = 10,
 
-        Texture = "Objects/FineHay", // provide spritesheet at assets/Objects/FineHay.png (16x16 per index)
-        SpriteIndex = 0,
-        
+        Texture = "Objects/FineHay",
+        // SpriteIndex = 0,
+
         Edibility = -300,   // Inedible
         // Fragility = 0,
         IsDrink = false,
